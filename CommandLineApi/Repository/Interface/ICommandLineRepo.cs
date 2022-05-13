8ff2e249adaa63b96f92ed453a210c326865f40d
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-13 22:32:03 +0100

    created method signature for interface

--- CommandLineApi/Repository/Interface/ICommandLineRepo.cs
+++ CommandLineApi/Repository/Interface/ICommandLineRepo.cs
@@ -1,8 +1,12 @@
 
+using System.Collections.Generic;
+using CommandLineApi.Models;
+
 namespace CommandLineApi.Repository
 {
     public interface ICommandLine
     {
-        
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int Id);
     }
 }
