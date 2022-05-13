f6b24c01ee21174cbeb33a37fe2f8e5741e46b5c
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-13 23:14:00 +0100

    carried out data seeding

--- CommandLineApi/Repository/Implementation/CommandLineRepo.cs
+++ CommandLineApi/Repository/Implementation/CommandLineRepo.cs
@@ -9,12 +9,38 @@ namespace CommandLineApi.Repository
     {
         public IEnumerable<Command> GetAllCommands()
         {
-            throw new System.NotImplementedException();
+            var commands = new List<Command>
+            {
+                new Command 
+                {
+                    CommandId      = 1,
+                    CommandCode    = "world",
+                    CommandUsage   = "hello world",
+                    UseEnvironment = "hello",
+                    OS             = "all" 
+                },
+
+                new Command 
+                {
+                    CommandId      = 2,
+                    CommandCode    = "osi",
+                    CommandUsage   = "hello osi",
+                    UseEnvironment = "hello",
+                    OS             = "all"
+                }
+            };
         }
 
         public Command GetCommandById(int Id)
         {
-            throw new System.NotImplementedException();
+            return new Command
+            {
+                CommandId      = 1,
+                CommandCode    = "world",
+                CommandUsage   = "hello world",
+                UseEnvironment = "hello",
+                OS             = "all" 
+            };
         }
     }
 }
