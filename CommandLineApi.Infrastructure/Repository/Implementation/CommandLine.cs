4ed16e6eaf7867ddf918c83aed3898f6e4d08bd1
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-14 23:27:15 +0100

    added implemetation and interface for the repository folder

---
+++ CommandLineApi.Infrastructure/Repository/Implementation/CommandLine.cs
@@ -0,0 +1,60 @@
+﻿using CommandLineApi.Infrastructure.Models;
+using CommandLineApi.Infrastructure.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApi.Infrastructure.Repository.Implementation
+{
+    public class CommandLine : ICommandLine
+    {
+        public IEnumerable<Command> GetAllCommand()
+        {
+            var commands = new List<Command>()
+            {
+               new Command()
+               {
+                   CommandId = 1,
+                CommandName = "Add Migration",
+                CommandSnippet = "Add-Migration [Migration_Name]",
+                OS = "All",
+                TargetEnvironment = "Entity FrameWork Core"
+               },
+
+               new Command()
+               {
+                   CommandId = 2,
+                   CommandName = "Update Database",
+                   CommandSnippet= "Update-Migration",
+                   TargetEnvironment = "Entity Framework Core",
+                   OS = "All"
+               },
+
+               new Command()
+               {
+                   CommandId = 3,
+                   CommandName = "Drop database",
+                   CommandSnippet = "Drop-Databse",
+                   TargetEnvironment = "Entity Framework Core",
+                   OS="All"
+               }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            var command = new Command()
+            {
+                CommandId = 1,
+                CommandName = "Add Migration",
+                CommandSnippet = "Add-Migration [Migration_Name]",
+                OS = "All",
+                TargetEnvironment = "Entity FrameWork Core"
+            };
+            return command;
+        }
+    }
+}
---
+++ CommandLineApi.Infrastructure/Repository/Interface/ICommandLine.cs
@@ -0,0 +1,16 @@
+﻿using CommandLineApi.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApi.Infrastructure.Repository.Interface
+{
+    public interface ICommandLine
+    {
+        Command GetCommandById(int Id);
+
+        IEnumerable<Command> GetAllCommand();
+    }
+}
