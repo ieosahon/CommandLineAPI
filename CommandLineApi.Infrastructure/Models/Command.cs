1a14699e435b83d313c8ed22b330fa368219a73c
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-14 22:46:30 +0100

    created the model entity

---
+++ CommandLineApi.Infrastructure/Models/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApi.Infrastructure.Models
+{
+    public class Command
+    {
+        public int CommandId { get; set; }
+        public string CommandName { get; set; }
+        public string CommandSnippet { get; set; }
+        public string TargetEnvironment { get; set; }
+        public string OS { get; set; }
+
+    }
+}
