f2b14972c5d51fc79f63fbce6d513823bdc4823b
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-20 06:17:47 +0100

    Added DbContext class

---
+++ CommandLineApi.Infrastructure/Context/CommandLineDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CommandLineApi.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApi.Infrastructure.Context
+{
+    public class CommandLineDbContext : DbContext
+    {
+        public CommandLineDbContext(DbContextOptions<CommandLineDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
