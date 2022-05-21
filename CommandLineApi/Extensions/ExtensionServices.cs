cc230c7bb7978e756554a7922ea313855ae46cf1
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-21 04:34:46 +0100

    injected db connection string

--- CommandLineApi/Extensions/ExtensionServices.cs
+++ CommandLineApi/Extensions/ExtensionServices.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using CommandLineApi.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CommandLineApi.Extensions
 {
     public static class ExtensionServices
     {
-        public static void DbContextConfiguration(this IServiceProvider serviceProvider)
-        {
-
-        }
+        public static void DbContextConfiguration(this IServiceCollection service, IConfiguration configuration) => service.AddDbContext<CommandLineDbContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("DbConn"), db => db.MigrationsAssembly("CommandLineApi")));
 
     }
 }
--- CommandLineApi/Startup.cs
+++ CommandLineApi/Startup.cs
@@ -1,3 +1,4 @@
+using CommandLineApi.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace CommandLineApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // injecting the db connection string
+            services.DbContextConfiguration(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
