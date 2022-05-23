53433d8f149271b39ac24b50a6f104ef6612e3f5
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-23 03:21:13 +0100

    added dto folder and files

---
+++ CommandLineApi.Core/DTO/CommandRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApi.Core.DTO
+{
+    public class CommandRequestDto
+    {
+    }
+}
---
+++ CommandLineApi.Core/Services/Implementations/CommandService.cs
@@ -0,0 +1,13 @@
+﻿using CommandLineApi.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApi.Core.Services.Implementations
+{
+    public class CommandService : ICommandService
+    {
+    }
+}
